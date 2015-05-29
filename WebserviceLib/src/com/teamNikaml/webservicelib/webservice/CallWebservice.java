5ceeefefa30da562a0d145dd4af0bb5c82699164
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2015-05-29 18:15:44 +0530

    init

---
+++ WebserviceLib/src/com/teamNikaml/webservicelib/webservice/CallWebservice.java
@@ -0,0 +1,174 @@
+package com.teamNikaml.webservicelib.webservice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class CallWebservice {
+
+	private static InputStream is = null;
+	private Context context;
+	//private static User user;
+    private  String URL;
+    private Map<String, String> parametersMap;
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String uRL) {
+		URL = uRL;
+	}
+
+	public Map<String, String> getParametersMap() {
+		return parametersMap;
+	}
+
+	public void setParametersMap(Map<String, String> parametersMap) {
+		this.parametersMap = parametersMap;
+	}
+
+	public CallWebservice(Context context) {
+		this.context = context;
+	}
+
+	public Context getContext() {
+		return context;
+	}
+
+	public void setContext(Context context) {
+		this.context = context;
+	}
+
+	public void doLogin() {
+		
+		
+
+		new LoginTask().execute("login");
+
+	}
+
+	
+
+	
+
+	private class LoginTask extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			int loginStatus = 0;
+			JSONObject json_data;
+			try {
+				json_data = new JSONObject(result);
+				for (int i = 0; i < json_data.length(); i++) {
+
+					if (Integer.parseInt(json_data.getString("status")) == 200) {
+						loginStatus = 200;
+					//	user.setUserKey(json_data.getString("userKey"));
+					//	user.setDesignation(json_data.getString("designation"));
+
+					} else
+						loginStatus = Integer.parseInt(json_data
+								.getString("status"));
+				}
+			
+
+			} catch (final JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if (loginStatus == 200) {
+				System.out.println("Login Sucessful");
+			
+			} else {
+				System.out.println("Login error");
+			}
+		}
+
+		@Override
+		protected String doInBackground(String... params) {
+
+			String json = "";
+			final List<BasicNameValuePair> params1 = new ArrayList<BasicNameValuePair>();
+		//	params1.add(new BasicNameValuePair("emailId", "abc@hm.cc"));
+		//	params1.add(new BasicNameValuePair("password","password123"));
+		//	params1.add(new BasicNameValuePair("requestKeyword", "Login"));
+			
+			
+			Iterator entries = parametersMap.entrySet().iterator();
+			while (entries.hasNext()) {
+			    Map.Entry entry = (Map.Entry) entries.next();
+			    String key = (String)entry.getKey();
+			    String value = (String)entry.getValue();
+			    System.out.println("Key = " + key + ", Value = " + value);
+			    
+			    params1.add(new BasicNameValuePair(key, value));
+			    
+			    
+			}
+			
+			
+			
+
+			try {
+
+				HttpPost httpPost = null;
+				final DefaultHttpClient httpClient = new DefaultHttpClient();
+				httpPost = new HttpPost(URL);
+				System.out.println(URL);
+				httpPost.setEntity(new UrlEncodedFormEntity(params1));
+				final HttpResponse httpResponse = httpClient.execute(httpPost);
+				final HttpEntity httpEntity = httpResponse.getEntity();
+
+				is = httpEntity.getContent();
+
+			} catch (final UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (final ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+
+			try {
+				final BufferedReader reader = new BufferedReader(
+						new InputStreamReader(is, "iso-8859-1"), 8);
+				final StringBuilder sb = new StringBuilder();
+				String line = null;
+				while ((line = reader.readLine()) != null) {
+					sb.append(line);
+				}
+				is.close();
+				json = sb.toString();
+				Log.e("JSON", json);
+			} catch (final Exception e) {
+				Log.e("Buffer Error", "Error converting result " + e.toString());
+			}
+
+			return json;
+		}
+	}
+
+}
