da3b8c9becb40b63822994a9fc1e8b0fc6cf1589
NikhilVijayakumar <nikhil.vijaykumar@gmail.com>
2015-06-10 13:37:23 +0530

    stream image from url

---
+++ WebserviceLib/src/com/teamNikaml/webservicelib/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.teamNikaml.webservicelib.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	
+	
+	//v.setBackgroundColor(context.getResources().getColor(R.color.enquiries_list_background));
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+	
+}
---
+++ WebserviceLib/src/com/teamNikaml/webservicelib/webservice/LoadImage.java
@@ -0,0 +1,78 @@
+/**
+ * @author Nikhil V
+ *
+ * Jun 10, 2015
+ */
+package com.teamNikaml.webservicelib.webservice;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.widget.ImageView;
+
+/**
+ * @author Nikhil V
+ * 
+ */
+public class LoadImage {
+
+	private String URL;
+	private ImageView myImageView;
+	private ProgressDialog pDialog;
+	private Context context;
+	private Bitmap bitmap;
+
+	public LoadImage(String uRL, ImageView myImageView, Context context) {
+		super();
+		URL = uRL;
+		this.myImageView = myImageView;
+		this.context = context;
+	}
+
+	public void getImage() {
+
+		new LoadImageAsyncTask().execute("LoadImage");
+
+	}
+
+	private class LoadImageAsyncTask extends AsyncTask<String, String, Bitmap> {
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(context);
+			pDialog.setMessage("Loading Image ....");
+			pDialog.show();
+
+		}
+
+		protected Bitmap doInBackground(String... args) {
+			try {
+				bitmap = BitmapFactory.decodeStream((InputStream) new URL(URL)
+						.getContent());
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return bitmap;
+		}
+
+		protected void onPostExecute(Bitmap image) {
+
+			if (image != null) {
+				bitmap = Bitmap.createScaledBitmap(bitmap, 1000, 700, false);
+
+				myImageView.setImageBitmap(bitmap);
+
+			}
+
+			pDialog.dismiss();
+
+		}
+	}
+
+}
